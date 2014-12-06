9177b12b3de14fc8121910086845edf80aa80d77
Andrea Salvadori <as0422@gmail.com>
2014-12-06 15:18:29 +0100

    ok2

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
 library(data.table)
 dt = fread("household_power_consumption.txt", header=TRUE, sep=";")
 dt = dt = dt[Date == c("1/2/2007","2/2/2007"),]
-library(ggplot2)
 hist(as.numeric(dt$Global_active_power), main="Global active power", xlab="Global active power (kilowatts)", ylab="Frequency", col="red", breaks=12) 
 dev.copy(png, file = "plot1.png")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,9 +1,8 @@
 library(data.table)
 dt = fread("/home/andrea/Progetti/R/ExData_Plotting1/household_power_consumption.txt", header=TRUE, sep=";")
 dt = dt = dt[Date == c("1/2/2007","2/2/2007"),]
-library(ggplot2)
 dt$DateTime = paste(dt$Date, dt$Time)
-plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Global_active_power), type='l', ylab="Global active power (kilowatts)")
+plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Global_active_power), type='l', xlab="", ylab="Global active power (kilowatts)")
 dev.copy(png, file = "/home/andrea/Progetti/R/ExData_Plotting1/plot2.png")
 dev.off()
 
