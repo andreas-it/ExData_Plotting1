28202767c806dff3584304d4fe53631d68199850
Andrea Salvadori <as0422@gmail.com>
2014-12-06 16:15:14 +0100

    ok3

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 library(data.table)
-dt = fread("/home/andrea/Progetti/R/ExData_Plotting1/household_power_consumption.txt", header=TRUE, sep=";")
+dt = fread("household_power_consumption.txt", header=TRUE, sep=";")
 dt = dt = dt[Date == c("1/2/2007","2/2/2007"),]
 dt$DateTime = paste(dt$Date, dt$Time)
 plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Global_active_power), type='l', xlab="", ylab="Global active power (kilowatts)")
-dev.copy(png, file = "/home/andrea/Progetti/R/ExData_Plotting1/plot2.png")
+dev.copy(png, file = "plot2.png")
 dev.off()
 
---
+++ plot3.R
@@ -0,0 +1,16 @@
+library(data.table)
+dt = fread("/home/andrea/Progetti/R/ExData_Plotting1/household_power_consumption.txt", header=TRUE, sep=";")
+dt = dt = dt[Date == c("1/2/2007","2/2/2007"),]
+dt$DateTime = paste(dt$Date, dt$Time)
+
+r= range(c(as.numeric(dt$Sub_metering_1), as.numeric(dt$Sub_metering_2), as.numeric(dt$Sub_metering_3)))
+plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Sub_metering_1), type='l', xlab="", ylab="Energy sub metering", col='black', ylim=r)
+par(new=T)
+plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Sub_metering_2), type='l', xlab='', ylab='', axes=F, col='red', ylim=r)
+par(new=T)
+plot(strptime(dt$DateTime, "%d/%m/%Y %H:%M:%S"), as.numeric(dt$Sub_metering_3), type='l', xlab='', ylab='', axes=F, col='blue', ylim=r)
+legend('topright', names(dt)[c(7,8,9)], lty=1, col=c('black', 'red', 'blue'), cex=0.75)
+
+dev.copy(png, file = "/home/andrea/Progetti/R/ExData_Plotting1/plot3.png")
+dev.off()
+
